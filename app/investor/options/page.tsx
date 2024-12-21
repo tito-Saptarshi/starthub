9a4de9dbf3c49334cb29598dff5498c4c2264a29
tito-Saptarshi <saptarshi.tito.20@gmail.com>
2024-12-21 19:57:57 +0530

    -m fix 0.3

--- app/investor/options/page.tsx
+++ app/investor/options/page.tsx
@@ -1,10 +1,12 @@
 import InvestorOptionsForm from "@/components/InvestorOptionsForm";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 
 
 export default async function InvestorOptionsPage() {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
+  if(!user) return redirect("/");
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-6 text-center">Investment Options</h1>
